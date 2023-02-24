8ee3e6ef9bf4d92142b25ec93efd50cc3e8c0057
noor_elmobasher <96498648+eng-noorelmobasher@users.noreply.github.com>
2023-02-24 07:05:53 +0200

    Revert "Add files via upload"
    This reverts commit 18da32524ed029c474d37b522505d3bba811b580.

--- Newcomers - Contest#2/A. Eng.Spidey.py
+++
@@ -1,7 +0,0 @@
-t = int(input())
-for _ in range(t):
-
-    n = int(input())
-    #if the polygon has 3 sides then the sum of angles is 180 and when we add 1 side the sum increases to 360!
-    #so we can see that (n-2)*180 would be the solution
-    print((n-2)*180)
--- Newcomers - Contest#2/F. Div 2 Contest.py
+++
@@ -1,6 +0,0 @@
-t = int(input())
-
-for _ in range(t):
-    
-    n = int(input())
-    print(2**n-1)
